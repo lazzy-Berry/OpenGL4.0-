f27cdc97946114fa52d83054019737ef4f7c6539
LazzyBerry <46856852+LazzyBerry@users.noreply.github.com>
2023-01-05 20:46:20 +0900

    * GLEWライブラリを追加 プロジェクトファイルを作成

---
+++ OpenGL/OpenGL.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
+
+int main()
+{
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cout << "Glewの初期化エラー: " << glewGetErrorString(err) << std::endl;
+    }
+    std::cout << "Hello World!\n";
+};
+
